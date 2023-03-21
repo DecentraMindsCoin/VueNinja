005259ae29f95629205a0e90b9a4a7c677799a8a
DecentraMindsCoin <decentraminds@gmail.com>
2023-03-20 22:48:36 -0700

    add for loop to map through books and add images to asset folder

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+      //   title: "The Final Empire",
+      //   author: "Brandon Sanderson",
+      //   age: 45,
       showBooks: true,
       x: 0,
-      y: 0
+      y: 0,
+      books: [
+        {
+          title: "name of the wind",
+          age: 45,
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          age: 45,
+          author: "bradon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          age: 21,
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
@@ -24,9 +47,9 @@ const app = Vue.createApp({
       }
     },
     handleMouseMove(e) {
-    this.x = e.offsetX
-    this.y = e.offsetY
-    }
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
   },
 });
 
