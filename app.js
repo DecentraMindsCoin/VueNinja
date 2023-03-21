c441fd74df80785f1af7f9f7756d2ff12c8a51f7
DecentraMindsCoin <decentraminds@gmail.com>
2023-03-20 17:05:13 -0700

    add some example book data and on click methods

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const app = Vue.createApp()
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+      showBooks: true,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      this.title = title;
+      // this.title = "The Hero of Ages";
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
